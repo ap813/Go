2a074c29ac1e30f2d414a028da60123a4488d1ed
Alex <aphillips813@gmail.com>
2018-08-26 16:29:02 -0400

    Bug Fix
    Wrong algorithm in one file

--- src/Sorting/InsertionSort/insertionSort.go
+++ src/Sorting/InsertionSort/insertionSort.go
@@ -9,12 +9,19 @@ func insertionSort(a [7]int, length int) [7]int {
 	// Sorting
 	for i := 1; i < length; i++ {
 
-		for j := 0; j < i; j++ {
+		for j := 0; j <= i; j++ {
 
-			if array[i] < array[j] {
+			if (array[i] < array[j]) {
 
-								
+				temp := array[j];
+				array[j] = array[i];
+				for k := j+1; k <= i ; k++ {
+					temp2 := array[k];
+					array[k] = temp;
+					temp = temp2;
+				}
 
+				j = i+1;
 			}
 
 		}
@@ -26,7 +33,7 @@ func insertionSort(a [7]int, length int) [7]int {
 
 func main() {
 
-	var array = [7]int{1,3,51,6,12,7,91};
+	var array = [7]int{1,5,12,8,2,4,5};
 
 	fmt.Println(insertionSort(array, 7));
 }
