ba8771571be3c2912d587d8439d8c33b4ffc9e75
Alex <aphillips813@gmail.com>
2018-08-28 12:24:04 -0400

    Simple Stack
    Stack

---
+++ src/Stacks/SimpleStack/stack.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+const MAX_SIZE int = 1000;
+
+type stack struct {
+	top int
+	array [MAX_SIZE]int
+}
+
+// Takes the value at the end of the array out
+func (s *stack) pop() int {
+
+	if s.top > 0 {
+		s.top--;
+		temp := s.array[s.top];
+		s.array[s.top] = 0;
+		return temp;
+	} else {
+		return 0;
+	}
+}
+
+// Puts the values of the at the position of top
+func (s *stack) push(num int) {
+	s.array[s.top] = num;
+	s.top++;
+	return;
+}
+
+func createStack() *stack {
+
+	s := new(stack);
+
+	s.top = 0;
+
+	return s;
+}
+
+func main() {
+
+	stack := createStack();
+
+	stack.push(10);
+	stack.push(20);
+	stack.push(30);
+	fmt.Println(stack.pop());
+}
