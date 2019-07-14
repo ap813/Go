0298d459591dd689bc325aff598595d9728341b2
Alex Phillips <alexphillips@Alexs-MacBook-Air.local>
2019-07-14 14:56:58 -0400

    Better answer

--- src/DynamicProgramming/ConsecutiveIntegerSum/sum.go
+++ src/DynamicProgramming/ConsecutiveIntegerSum/sum.go
@@ -1,15 +1,5 @@
 package main
 
-/*
-	From: Algorithms by Dasgupta, C. H. Papadimitriou, and U. V. Vazirani
-	A contiguous subsequence of a list S is a subsequence made up of consecutive elements of S. For instance, if S is
-	5, 15, −30, 10, −5, 40, 10,
-	then 15, −30, 10 is a contiguous subsequence but 5, 15, 40 is not. Give a linear-time algorithm for
-	the following task:
-	Input: A list of numbers, a1, a2, . . . , an.
-	Output: The contiguous subsequence of maximum sum (a subsequence of length zero has sum zero).
-	For the preceding example, the answer would be 10, −5, 40, 10, with a sum of 55.
-*/
 import (
 	"fmt"
 )
@@ -26,16 +16,12 @@ func main() {
 
 		sum := conseq[i-1] + array[i]
 
-		if sum > conseq[i] {
+		if sum > 0 {
 			max = sum
 			conseq[i] = max
 		} else {
-			conseq[i] = array[i]
+			conseq[i] = 0
 		}
-	}
-
-	if max < 0 {
-		max = 0
 	}
 
 	fmt.Println(max)
